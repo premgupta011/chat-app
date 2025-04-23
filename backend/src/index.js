62b31b6fb46dda1cfe3917dbe452104ffc7cf984
premgupta011 <premgupta0807@gmail.com>
2025-04-23 17:57:59 +0530

    backend login-signup created

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,18 @@
 // const express = require("express")
-import express from "express"
 import dotenv from "dotenv"
+import express from "express"
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js"
+import cookieParse from "cookie-parser";
 
 dotenv.config();
 const app = express();
- const PORT = process.env.PORT;
+const PORT = process.env.PORT;
+app.use(express.json());
+app.use(cookieParse());
+app.get("/",(req, res)=>{
+    res.send(`it is working`);
+ })
 app.use("/api/auth", authRoutes);
 app.listen(PORT, ()=>{
     console.log(`server is running on ${PORT}`)
